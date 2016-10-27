465ebe6d30ca70b0d71875825dd64ea16c588501
topalek <topalek@mail.ru>
2016-10-27 14:36:27 +0300

    add php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="footer-content">
+					<a href="/"><span>“Жизнь прекрасна”</span> -  агентство торжеств</a>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="footer-contacts">
+					<div class="header_email pull-right">
+						<i class="icon">
+							<img src="img/svg/evelope-white.svg" alt="phone-ringing">
+						</i>
+						<a href="mailto:markiza.anna@gmail.com">markiza.anna@gmail.com</a>
+					</div>
+					<div class="pull-right">
+						<div class="header_phone">
+							<i class="icon">
+								<img src="img/svg/phone-ringing-white.svg" alt="phone-ringing">
+							</i>
+							<a href="tel:+380504708819">+38 (050) 470 88 19</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,94 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="robots" content="noindex,nofollow">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Life is Wonderful</title>
+	<link rel="stylesheet" href="css/font-awesome.min.css">
+	<link rel="stylesheet" href="css/bootstrap.css">
+	<link rel="stylesheet" href="css/owl.carousel.css">
+	<link rel="stylesheet" href="css/style.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/modernizr-custom.js"></script>
+	<script src="js/owl.carousel.js"></script>
+	<script src="js/app.js"></script>
+</head>
+<body>
+<header class="header">
+	<div id="header_top">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7"></div>
+				<div class="col-md-2 no-padding">
+					<div class="pull-right">
+						<div class="header_phone">
+							<i class="icon">
+								<img src="img/svg/phone-ringing.svg" alt="phone-ringing">
+							</i>
+							<a href="tel:+380504708819">+38 (050) 470 88 19</a>
+						</div>
+					</div>
+
+				</div>
+				<div class="col-md-3">
+					<div class="header_email pull-right">
+						<i class="icon">
+							<img src="img/svg/evelope.svg" alt="phone-ringing">
+						</i>
+						<a href="mailto:markiza.anna@gmail.com">markiza.anna@gmail.com</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="header_nav">
+		<nav>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="nav-container clearfix">
+							<div class="nav-item">
+								<a href="/" class="logo text-xs-center">
+									<div class="logo_header">жизнь прекрасна</div>
+									<div class="logo_description">агентство торжеств</div>
+								</a>
+							</div>
+							<div class="nav-item">
+								<ul class="nav">
+									<li><a href="/" class="nav-link active">главная</a></li>
+									<li><a href="service.html">услуги</a></li>
+									<li><a href="reviews.html">отзывы</a></li>
+									<li><a href="portfolio.html">портфолио</a></li>
+									<li><a href="contacts.html">контакты</a></li>
+								</ul>
+							</div>
+							<div class="nav-item">
+								<ul class="link_group">
+									<li><a href="#" class="link">Театр песни "<strong>Сапфир</strong>"</a></li>
+									<li><a href="#" class="link">Кавер группа "<strong>Carre</strong>"</a></li>
+								</ul>
+							</div>
+
+							<div class="nav-item">
+								<div class="search_triger">
+									<i class="fa fa-search"></i>
+								</div>
+							</div>
+							<div class="header_search">
+								<form action="" class="form-search">
+									<input type="text" class="input-search" name="s" placeholder="Например: прокат лимузина">
+									<button class="input-submit" title="Найти">
+										<i class="fa fa-search"></i>
+									</button>
+									<div id="close-search"><i class="fa fa-close"></i></div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	</div>
