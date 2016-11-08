d2e66655ca46006ba15c1fb8933a37c9b702ff8c
topalek <topalek@mail.ru>
2016-11-08 09:44:51 +0200

    начата страничка Карре

--- carre.php
+++ carre.php
@@ -1,1 +1,50 @@
-carre
+<section id="carre-page-hero" class="hero wow fadeIn">
+	<div class="row">
+		<div class="col-md-12 no-padding">
+			<div class="carre-page-hero_img">
+				<div class="nets">
+					<ul class="social">
+						<li><a href="#" class="net_link"><i class="fa fa-facebook-f"></i></a></li>
+						<li><a href="#" class="net_link"><i class="fa fa-vk"></i></a></li>
+						<li><a href="#" class="net_link"><i class="fa fa-youtube-play"></i></a></li>
+						<li><a href="#" class="net_link"><i class="fa fa-instagram"></i></a></li>
+					</ul>
+				</div>
+				<div class="page_title text-xs-center">
+					<h1 class="title">Зажигательный cover band<br>
+						для Вашего идеального события!</h1>
+					<span class="sep"></span>
+					<h2 class="description">Живое общение с публикой через музыку</h2>
+					<a href="#" class="my-btn" data-toggle="modal" data-target="#formModal">Заказать консультацию организатора событий</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<section id="about" class="arr_top">
+	<div class="row">
+		<div class="col-md-12 no-padding">
+			<div class="contacts-page-info">
+				<div class="contacts-page_title text-xs-center">
+					<h2 class="title text-uppercase">Репертуар</h2>
+					<span class="sep"></span>
+					<h3 class="slogan">Репертуар кавер-группы “Carre” мультижанровый: популярные песни,<br>
+						народные, казацкие, ретро, авторские, а-капелла, инструментальные,<br>
+						вокально-инструментальные композиции.</h3>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-12 text-xs-center">
+					<div class="col-md-6">
+						<a href="#" class="my-btn ghost p-25">Стотреть плей-лист <span> ></span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</section>
